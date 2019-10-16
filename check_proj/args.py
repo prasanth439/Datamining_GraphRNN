4ec1948ebcfaed8cc3b761e5b07a3349aa4482d8
Anirudh8841 <anirudh8841@gmail.com>
2019-10-17 01:44:47 +0530

    commited

--- check_proj/args.py
+++ check_proj/args.py
@@ -23,8 +23,7 @@ class Args():
         # self.graph_type = 'caveman_small'
         # self.graph_type = 'caveman_small_single'
         # self.graph_type = 'community4'
-        # self.graph_type = 'grid'
-        self.graph_type = 'new_full'
+        self.graph_type = 'grid'
         # self.graph_type = 'grid_small'
         # self.graph_type = 'ladder_small'
 
@@ -65,17 +64,12 @@ class Args():
         ### training config
         self.num_workers = 4 # num workers to load data, default 4
         self.batch_ratio = 32 # how many batches of samples per epoch, default 32, e.g., 1 epoch = 32 batches
-        # self.epochs = 3000 # now one epoch means self.batch_ratio x batch_size
-        # self.epochs_test_start = 100
-        # self.epochs_test = 100
-        # self.epochs_log = 100
-        # self.epochs_save = 100
-        self.batch_ratio = 1 # how many batches of samples per epoch, default 32, e.g., 1 epoch = 32 batches
-        self.epochs = 30 # now one epoch means self.batch_ratio x batch_size
-        self.epochs_test_start = 10
-        self.epochs_test = 10
-        self.epochs_log = 10
-        self.epochs_save = 5
+        self.epochs = 3000 # now one epoch means self.batch_ratio x batch_size
+        self.epochs_test_start = 100
+        self.epochs_test = 100
+        self.epochs_log = 100
+        self.epochs_save = 100
+
         self.lr = 0.003
         self.milestones = [400, 1000]
         self.lr_rate = 0.3
