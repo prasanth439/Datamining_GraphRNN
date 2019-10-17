4239819491099fe523ef361805316e182b998343
Anirudh8841 <anirudh8841@gmail.com>
2019-10-18 00:06:48 +0530

    commited

--- check_proj/main.py
+++ check_proj/main.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
     # graphs = create_graphs.create(args.input_file)
-    graphs = convert_to_networkGraphs(input_file)
-    
+    graphs = convert_to_networkGraphs(args.input_file)
+
     if not os.path.isdir(args.model_save_path):
         os.makedirs(args.model_save_path)
     if not os.path.isdir(args.graph_save_path):
